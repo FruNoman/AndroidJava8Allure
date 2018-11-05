944e962f373ecafa26bfd91301270f8c7f0a2cfb
Dmytro A Frolov <dmytro.a.frolov@globallogic.com>
2018-11-05 14:22:10 +0200

    add before after test step aspect

--- app/src/androidTest/java/com/example/dfrolov/allureandroidjava8/SomeAspect.java
+++ app/src/androidTest/java/com/example/dfrolov/allureandroidjava8/SomeAspect.java
@@ -1,26 +1,107 @@
 package com.example.dfrolov.allureandroidjava8;
+
+import com.example.dfrolov.allureandroidjava8.allure_implementation.allure.Allure;
+import com.example.dfrolov.allureandroidjava8.allure_implementation.allure.AllureLifecycle;
+import com.example.dfrolov.allureandroidjava8.allure_implementation.model_pojo.Status;
+import com.example.dfrolov.allureandroidjava8.allure_implementation.model_pojo.StepResult;
+
+import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.example.dfrolov.allureandroidjava8.allure_implementation.allure.util.AspectUtils.getParameters;
+import static com.example.dfrolov.allureandroidjava8.allure_implementation.allure.util.AspectUtils.getParametersMap;
+import static com.example.dfrolov.allureandroidjava8.allure_implementation.allure.util.NamingUtils.processNameTemplate;
+import static com.example.dfrolov.allureandroidjava8.allure_implementation.allure.util.ResultsUtils.getStatusDetails;
+import static com.example.dfrolov.allureandroidjava8.allure_implementation.allure.util.ResultsUtils.getStatusNot;
+
 @Aspect
 public class SomeAspect {
 
+    private static AllureLifecycle lifecycle;
 
-
-
-    public SomeAspect() {
-    }
-
-
-    @Around("execution(* com.example.dfrolov.allureandroidjava8..*.*(..))")
-    public Object step(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Around("@annotation(org.junit.Before)")
+    public Object beforeTest(ProceedingJoinPoint joinPoint) throws Throwable {
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
 
-        System.out.println(" BEFORE METHOD "+methodSignature.getDeclaringType().getSimpleName()+"   "+methodSignature.getMethod().getName());
-        Object proceed = joinPoint.proceed();
-        System.out.println(" AFTER METHOD "+methodSignature.getDeclaringType().getSimpleName()+"   "+methodSignature.getMethod().getName());
+        final String uuid = UUID.randomUUID().toString();
+        final String name = "Before test";
+
+        lifecycle = Allure.getLifecycle();
+
+        final StepResult result = new StepResult()
+                .withName(name)
+                .withStatus(Status.PASSED)
+                .withParameters(getParameters(methodSignature, joinPoint.getArgs()));
+        lifecycle.startStep(uuid, result);
+        final Object proceed = joinPoint.proceed();
+        lifecycle.stopStep(uuid);
+
+        return proceed;
+    }
+
+    @Around("@annotation(org.junit.After)")
+    public Object afterTest(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+
+        final String uuid = UUID.randomUUID().toString();
+        final String name = "After test";
+
+        lifecycle = Allure.getLifecycle();
+
+        final StepResult result = new StepResult()
+                .withName(name)
+                .withStatus(Status.PASSED)
+                .withParameters(getParameters(methodSignature, joinPoint.getArgs()));
+        lifecycle.startStep(uuid, result);
+        final Object proceed = joinPoint.proceed();
+        lifecycle.stopStep(uuid);
+
+        return proceed;
+    }
+
+    @Around("@annotation(org.junit.Test)")
+    public Object test(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+
+        final String uuid = UUID.randomUUID().toString();
+        final String name = "Test";
+
+        lifecycle = Allure.getLifecycle();
+
+        final StepResult result = new StepResult()
+                .withName(name)
+                .withStatus(Status.PASSED)
+                .withParameters(getParameters(methodSignature, joinPoint.getArgs()));
+        lifecycle.startStep(uuid, result);
+        final Object proceed = joinPoint.proceed();
+        lifecycle.stopStep(uuid);
+
+        return proceed;
+    }
+
+    @Around("@annotation(org.junit.Test)")
+    public Object anotherStep(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+
+        final String uuid = UUID.randomUUID().toString();
+        final String name = "Test";
+
+        lifecycle = Allure.getLifecycle();
+
+        final StepResult result = new StepResult()
+                .withName(name)
+                .withStatus(Status.PASSED)
+                .withParameters(getParameters(methodSignature, joinPoint.getArgs()));
+        lifecycle.startStep(uuid, result);
+        final Object proceed = joinPoint.proceed();
+        lifecycle.stopStep(uuid);
+
         return proceed;
     }
 }
---
+++ app/src/androidTest/java/com/example/dfrolov/allureandroidjava8/tests/ExampleInstrumentedTest.java
@@ -0,0 +1,73 @@
+package com.example.dfrolov.allureandroidjava8.tests;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.support.test.rule.GrantPermissionRule;
+
+
+import com.example.dfrolov.allureandroidjava8.Some;
+import com.example.dfrolov.allureandroidjava8.allure_implementation.RenesasRunner;
+import com.example.dfrolov.allureandroidjava8.allure_implementation.allure.Severity;
+import com.example.dfrolov.allureandroidjava8.allure_implementation.allure.SeverityLevel;
+import com.example.dfrolov.allureandroidjava8.allure_implementation.junit4.DisplayName;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(RenesasRunner.class)
+public class ExampleInstrumentedTest {
+    private Some some;
+    @Rule
+    public GrantPermissionRule permissionsRules =
+            GrantPermissionRule.grant(
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+
+    @Before
+    public void beforeSome(){
+        some = new Some();
+        some.doSome();
+    }
+
+    @DisplayName("Aspectj test")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        adapter.enable();
+
+        some.doSome();
+        some.doSome();
+        some.doSome();
+    }
+
+    @DisplayName("Aspectj test2")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test
+    public void useAppContext1() {
+        // Context of the app under test.
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        adapter.enable();
+
+        some.doSome();
+        some.doSome();
+        some.doSome();
+    }
+
+    @After
+    public void afterSome(){
+        some.doSome();
+    }
+}
