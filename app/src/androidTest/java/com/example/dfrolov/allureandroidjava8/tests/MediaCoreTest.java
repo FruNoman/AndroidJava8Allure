af082e98210e8cc3b0281786cf266addc09148c8
Dmytro A Frolov <dmytro.a.frolov@globallogic.com>
2018-11-13 13:46:33 +0200

    add video tests

--- app/src/androidTest/java/com/example/dfrolov/allureandroidjava8/tests/MediaCoreTest.java
+++ app/src/androidTest/java/com/example/dfrolov/allureandroidjava8/tests/MediaCoreTest.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
+import android.os.Looper;
 import android.os.SystemClock;
 import android.support.test.annotation.UiThreadTest;
 import android.support.test.rule.GrantPermissionRule;
@@ -35,8 +36,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import static junit.framework.Assert.assertNotSame;
 import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
 
 
 @Epic("Media core features")
@@ -51,21 +52,185 @@ public class MediaCoreTest extends BaseTest {
     private String IMAGE_FOLDER_PATH = "/sdcard/samples/image/";
     private String VIDEO_FOLDER_PATH = "/sdcard/samples/video/";
     private String AUDIO_FOLDER_PATH = "/sdcard/samples/audio/";
+    private int WAIT_FOR_COMMAND_TO_COMPLETE = 60000;  //1 min max.
+    private boolean mPrepareReset = false;
+    private Looper mLooper = null;
+    private final Object mLock = new Object();
+    private final Object mPrepareDone = new Object();
+    private final Object mOnCompletion = new Object();
+    private final long PAUSE_WAIT_TIME = 2000;
+    private final long WAIT_TIME = 2000;
+    private final int SEEK_TIME = 10000;
+    private boolean mInitialized = false;
 
-    public void testImage(File image) throws FileNotFoundException {
+    private boolean mOnPrepareSuccess = false;
+    public boolean mOnCompleteSuccess = false;
+    public boolean mPlaybackError = false;
+    public int mMediaInfoUnknownCount = 0;
+    public int mMediaInfoVideoTrackLaggingCount = 0;
+    public int mMediaInfoBadInterleavingCount = 0;
+    public int mMediaInfoNotSeekableCount = 0;
+    public int mMediaInfoMetdataUpdateCount = 0;
+
+
+    public void testImage(File image) throws FileNotFoundException, SkipExeption {
+        if (!image.exists()) {
+            throw new SkipExeption("Can't find sample " + image.getPath());
+        }
         TestUtils.setTitle(activity, image.getName());
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inPreferredConfig = Bitmap.Config.ARGB_8888;
         final Bitmap bitmap = BitmapFactory.decodeStream(new FileInputStream(image), null, options);
-        int beforeH=imageView.getMeasuredHeight();
+        int beforeH = imageView.getMeasuredHeight();
         int beforeW = imageView.getMeasuredWidth();
         activity.setImageView(bitmap);
         SystemClock.sleep(5000);
-        int afterH=imageView.getMeasuredHeight();
+        int afterH = imageView.getMeasuredHeight();
         int afterW = imageView.getMeasuredWidth();
-        assertNotSame("imageView Height not changed after set src",beforeH, afterH);
-        assertNotSame("imageView Width not changed after set src",beforeW, afterW);
-        assertTrue("imageView size incorrect",(afterH>100 && afterW>100));
+        Assert.assertNotSame("imageView Height not changed after set src", beforeH, afterH);
+        Assert.assertNotSame("imageView Width not changed after set src", beforeW, afterW);
+        Assert.assertTrue("imageView size incorrect", (afterH > 100 && afterW > 100));
+    }
+
+    private void initializeMessageLooper() {
+        new Thread() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                mLooper = Looper.myLooper();
+                mediaPlayer = new MediaPlayer();
+                synchronized (mLock) {
+                    mInitialized = true;
+                    mLock.notify();
+                }
+                Looper.loop();
+            }
+        }.start();
+    }
+
+    private void terminateMessageLooper() {
+        mLooper.quit();
+        mediaPlayer.release();
+    }
+
+    MediaPlayer.OnPreparedListener mPreparedListener = new MediaPlayer.OnPreparedListener() {
+        public void onPrepared(MediaPlayer mp) {
+            synchronized (mPrepareDone) {
+                if (mPrepareReset) {
+                    mediaPlayer.reset();
+                }
+                mPrepareDone.notify();
+                mOnPrepareSuccess = true;
+            }
+        }
+    };
+    MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        public void onCompletion(MediaPlayer mp) {
+            synchronized (mOnCompletion) {
+                mOnCompletion.notify();
+                mOnCompleteSuccess = true;
+            }
+        }
+    };
+
+    MediaPlayer.OnErrorListener mOnErrorListener = new MediaPlayer.OnErrorListener() {
+        public boolean onError(MediaPlayer mp, int framework_err, int impl_err) {
+            mPlaybackError = true;
+            mp.reset();
+            synchronized (mOnCompletion) {
+                mOnCompletion.notify();
+                mOnCompleteSuccess = false;
+            }
+            Assert.assertTrue(false);
+            return true;
+        }
+    };
+
+    MediaPlayer.OnInfoListener mInfoListener = new MediaPlayer.OnInfoListener() {
+        public boolean onInfo(MediaPlayer mp, int what, int extra) {
+            switch (what) {
+                case MediaPlayer.MEDIA_INFO_UNKNOWN:
+                    mMediaInfoUnknownCount++;
+                    break;
+                case MediaPlayer.MEDIA_INFO_VIDEO_TRACK_LAGGING:
+                    mMediaInfoVideoTrackLaggingCount++;
+                    Assert.assertTrue(false);
+
+                    break;
+                case MediaPlayer.MEDIA_INFO_BAD_INTERLEAVING:
+                    mMediaInfoBadInterleavingCount++;
+                    Assert.assertTrue(false);
+
+                    break;
+                case MediaPlayer.MEDIA_INFO_NOT_SEEKABLE:
+                    mMediaInfoNotSeekableCount++;
+                    Assert.assertTrue(false);
+                    break;
+                case MediaPlayer.MEDIA_INFO_METADATA_UPDATE:
+                    mMediaInfoMetdataUpdateCount++;
+                    break;
+            }
+            return true;
+        }
+    };
+
+    public void testMedia(File video) throws SkipExeption {
+        if (!video.exists()) {
+            throw new SkipExeption("Can't find sample " + video.getPath());
+        }
+        int duration = 0;
+        int waittime = 0;
+        mOnCompleteSuccess = false;
+        mMediaInfoUnknownCount = 0;
+        mMediaInfoVideoTrackLaggingCount = 0;
+        mMediaInfoBadInterleavingCount = 0;
+        mMediaInfoNotSeekableCount = 0;
+        mMediaInfoMetdataUpdateCount = 0;
+        mPlaybackError = false;
+        initializeMessageLooper();
+        synchronized (mLock) {
+            try {
+                mLock.wait(WAIT_FOR_COMMAND_TO_COMPLETE);
+            } catch (Exception e) {
+                Assert.assertTrue(false);
+            }
+        }
+        try {
+            mediaPlayer.setOnCompletionListener(mCompletionListener);
+            mediaPlayer.setOnErrorListener(mOnErrorListener);
+            mediaPlayer.setOnInfoListener(mInfoListener);
+            mediaPlayer.setDataSource(video.getAbsolutePath());
+            TestUtils.setTitle(activity, video.getName());
+            mediaPlayer.setDisplay(surfaceView.getHolder());
+            mediaPlayer.prepare();
+            duration = mediaPlayer.getDuration();
+            mediaPlayer.start();
+            int lastSeek = duration * 8 / 10;
+            mediaPlayer.seekTo(lastSeek);
+
+            int currentPosition = mediaPlayer.getCurrentPosition();
+            Assert.assertTrue(currentPosition > lastSeek * 0.8);
+            waittime = duration - mediaPlayer.getCurrentPosition();
+            SystemClock.sleep(1000);
+
+            synchronized (mOnCompletion) {
+                try {
+                    mOnCompletion.wait(waittime + 3000);
+                } catch (Exception e) {
+                    Assert.assertTrue(false);
+                }
+            }
+            //mMediaPlayer.stop();
+
+
+            Assert.assertTrue(mOnCompleteSuccess);
+            terminateMessageLooper();
+            if (activity != null) activity.finish();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.assertTrue(false);
+        }
     }
 
     @Rule
@@ -91,7 +256,7 @@ public class MediaCoreTest extends BaseTest {
         File videoFolder = new File(VIDEO_FOLDER_PATH);
         File audioFolder = new File(AUDIO_FOLDER_PATH);
 
-        if(!imageFolder.exists() || !videoFolder.exists() || !audioFolder.exists()){
+        if (!imageFolder.exists() || !videoFolder.exists() || !audioFolder.exists()) {
             throw new SkipExeption("Can't find samples folders");
         }
 
@@ -100,88 +265,143 @@ public class MediaCoreTest extends BaseTest {
     @DisplayName("Image format .jpg I000287 tests")
     @Severity(SeverityLevel.BLOCKER)
     @Test
-    public void testJpegI000287() throws FileNotFoundException {
+    public void testJpegI000287() throws FileNotFoundException, SkipExeption {
         testImage(new File(IMAGE_FOLDER_PATH + Samples.jpgI000287.toString()));
     }
 
     @DisplayName("Image format .jpg I000309 tests")
     @Severity(SeverityLevel.BLOCKER)
     @Test
-    public void testJpgI000309() throws FileNotFoundException {
+    public void testJpgI000309() throws FileNotFoundException, SkipExeption {
         testImage(new File(IMAGE_FOLDER_PATH + Samples.jpgI000309.toString()));
     }
 
     @DisplayName("Image format .jpg I000311 tests")
     @Severity(SeverityLevel.BLOCKER)
     @Test
-    public void testJpgI000311() throws FileNotFoundException {
+    public void testJpgI000311() throws FileNotFoundException, SkipExeption {
         testImage(new File(IMAGE_FOLDER_PATH + Samples.jpgI000311.toString()));
     }
 
     @DisplayName("Image format .jpg I000001 tests")
     @Severity(SeverityLevel.BLOCKER)
     @Test
-    public void testJpgI000001() throws FileNotFoundException {
+    public void testJpgI000001() throws FileNotFoundException, SkipExeption {
         testImage(new File(IMAGE_FOLDER_PATH + Samples.jpgI000001.toString()));
     }
 
     @DisplayName("Image format .webp I000308 tests")
     @Severity(SeverityLevel.BLOCKER)
     @Test
-    public void testWebpI000308() throws FileNotFoundException {
+    public void testWebpI000308() throws FileNotFoundException, SkipExeption {
         testImage(new File(IMAGE_FOLDER_PATH + Samples.webpI000308.toString()));
     }
 
     @DisplayName("Image format .gif I000021 tests")
     @Severity(SeverityLevel.BLOCKER)
     @Test
-    public void testGifI000021() throws FileNotFoundException {
+    public void testGifI000021() throws FileNotFoundException, SkipExeption {
         testImage(new File(IMAGE_FOLDER_PATH + Samples.gifI000021.toString()));
     }
 
     @DisplayName("Image format .bmp I000312 tests")
     @Severity(SeverityLevel.BLOCKER)
     @Test
-    public void testBmpI000312() throws FileNotFoundException {
+    public void testBmpI000312() throws FileNotFoundException, SkipExeption {
         testImage(new File(IMAGE_FOLDER_PATH + Samples.bmpI000312.toString()));
     }
 
     @DisplayName("Image format .png I000301 tests")
     @Severity(SeverityLevel.BLOCKER)
     @Test
-    public void testPngI0003012() throws FileNotFoundException {
+    public void testPngI0003012() throws FileNotFoundException, SkipExeption {
         testImage(new File(IMAGE_FOLDER_PATH + Samples.pngI000301.toString()));
     }
 
     @DisplayName("Image format .png I000313 tests")
     @Severity(SeverityLevel.BLOCKER)
     @Test
-    public void testPngI000313() throws FileNotFoundException {
+    public void testPngI000313() throws FileNotFoundException, SkipExeption {
         testImage(new File(IMAGE_FOLDER_PATH + Samples.pngI000313.toString()));
     }
 
-
-
-    @DisplayName("Video formats displaying tests")
+    @DisplayName("Video format h263AV001248 tests")
     @Severity(SeverityLevel.BLOCKER)
-    @UiThreadTest
     @Test
-    public void videoFormatsTest() throws IOException, InterruptedException {
-        mediaPlayer = new MediaPlayer();
-        for (File video : new File(VIDEO_FOLDER_PATH).listFiles()) {
-            String path = video.getAbsolutePath();
-            activity.getSurfaceView();
-            mediaPlayer.setDisplay(surfaceView.getHolder());
-            mediaPlayer.setDataSource(path);
-            mediaPlayer.setLooping(true);
-            mediaPlayer.prepare();
-            mediaPlayer.start();
-            Thread.sleep(5000);
-            Assert.assertTrue(mediaPlayer.isPlaying());
-            mediaPlayer.reset();
-        }
-        mediaPlayer.stop();
+    public void testH263AV001248() throws SkipExeption {
+        testMedia(new File(VIDEO_FOLDER_PATH + Samples.h263AV001248.toString()));
     }
+
+    @DisplayName("Video format h264AV000869 tests")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test
+    public void testH264AV000869() throws SkipExeption {
+        testMedia(new File(VIDEO_FOLDER_PATH + Samples.h264AV000869.toString()));
+    }
+
+    @DisplayName("Video format h264AV001097 tests")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test
+    public void testH264AV001097() throws SkipExeption {
+        testMedia(new File(VIDEO_FOLDER_PATH + Samples.h264AV001097.toString()));
+    }
+
+    @DisplayName("Video format h264AV001187 tests")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test
+    public void testH264AV001187() throws SkipExeption {
+        testMedia(new File(VIDEO_FOLDER_PATH + Samples.h264AV001187.toString()));
+    }
+
+
+    @DisplayName("Video format h265AV001429 tests")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test
+    public void testH265AV001429() throws SkipExeption {
+        testMedia(new File(VIDEO_FOLDER_PATH + Samples.h265AV001429.toString()));
+    }
+
+    @DisplayName("Video format h265AV001430 tests")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test
+    public void testH265AV001430() throws SkipExeption {
+        testMedia(new File(VIDEO_FOLDER_PATH + Samples.h265AV001430.toString()));
+    }
+
+    @DisplayName("Video format h265AV001431 tests")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test
+    public void testH265AV001431() throws SkipExeption {
+        testMedia(new File(VIDEO_FOLDER_PATH + Samples.h265AV001431.toString()));
+    }
+
+    @DisplayName("Video format mpeg4AV000022 tests")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test
+    public void testMPEG4AV000022() throws SkipExeption {
+        testMedia(new File(VIDEO_FOLDER_PATH + Samples.mpeg4AV000022.toString()));
+    }
+
+//    @DisplayName("Video formats displaying tests")
+//    @Severity(SeverityLevel.BLOCKER)
+//    @UiThreadTest
+//    @Test
+//    public void videoFormatsTest() throws IOException, InterruptedException {
+//        mediaPlayer = new MediaPlayer();
+//        for (File video : new File(VIDEO_FOLDER_PATH).listFiles()) {
+//            String path = video.getAbsolutePath();
+//            activity.getSurfaceView();
+//            mediaPlayer.setDisplay(surfaceView.getHolder());
+//            mediaPlayer.setDataSource(path);
+//            mediaPlayer.setLooping(true);
+//            mediaPlayer.prepare();
+//            mediaPlayer.start();
+//            Thread.sleep(5000);
+//            Assert.assertTrue(mediaPlayer.isPlaying());
+//            mediaPlayer.reset();
+//        }
+//        mediaPlayer.stop();
+//    }
 
     @After
     public void afterTest() {
--- app/src/androidTest/java/com/example/dfrolov/allureandroidjava8/utils/Samples.java
+++ app/src/androidTest/java/com/example/dfrolov/allureandroidjava8/utils/Samples.java
@@ -1,6 +1,7 @@
 package com.example.dfrolov.allureandroidjava8.utils;
 
 public enum Samples {
+    // IMAGE SAMPLES ->
     jpgI000287("I_000287_benchmark_lightbox_5336x3000_16MP.jpg"),
     jpgI000309("I_000309_roskilde_station.jpg"),
     jpgI000310("I_000310_kettbach_dulmen.jpg"),
@@ -10,7 +11,28 @@ public enum Samples {
     gifI000021("I_000021_GPN_001040.gif"),
     bmpI000312("I_000312_moon_landscape.bmp"),
     pngI000301("I_000301_1922_world_map_34995x2374.png"),
-    pngI000313("I_000313_niagara_falls.png");
+    pngI000313("I_000313_niagara_falls.png"),
+
+
+    // VIDEO SAMPLES ->
+    h263AV001248("AV_001248_lunarsurface_H263_CIF_2mbps_30fps_NB_AMR.mp4"),
+    h264AV000869("AV_000869_H264_High_Profile_level_31_VGA_30fps_4Mbps_AAC_48kHz_128kbps_stereo.mp4"),
+    h264AV001097("AV_001097_1080p_crowdrun_HP_cabac_2B_wBpred_adct_30fps_20mbps_1000fr_AAC_HE_48kHz_64kbps_stereo.mp4"),
+    h264AV001187("AV_001187_Toy_Story3Official_Trailer_in_720p_h264_MP_L2_1280x720_24fps_1Mbps_eAACplus_64kbps_44100Hz.mp4"),
+    h265AV001429("AV_001429_life_in_orbit_720p_main_2.mp4"),
+    h265AV001430("AV_001430_iss_tour_720p_main_3.mp4"),
+    h265AV001431("AV_001431_arthur_christmas_720p_psnr_main_22.mp4"),
+    mpeg4AV000022("AV_000022_SF145.mp4"),
+    mp3A000001("A_000001_02_Tetanus.mp3"),
+    wavA000354("A_000354_PCM_16bit_48Khz_1536kbps_stereo.wav"),
+    flacA000484("A_000484_SpoonRevenge.flac"),
+    sample3gpA000123("A_000123_15dot85kbps_ex30.3gp"),
+    aacA000157("A_000157_128kbps_werk32.aac"),
+    amrA000443("A_000443_NB_AMR_8000Hz_10.2kbps.AMR"),
+    mp4A000454("A_000454_eAACplus_48000Hz_32Kbps_Stereo_track1.mp4"),
+    oggA000480("A_000480_The_Abyss-4T.ogg"),
+    mkaA000485("A_000485_ehren-paper_lights.mka");
+
 
     String path;
 
